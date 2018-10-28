40449f43188a4c9c82a0d8597ce3c06c6fa09c8d
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2018-10-28 17:03:10 +0100

    Configure tables with objects

--- src/components/JimmyContentTable.vue
+++ src/components/JimmyContentTable.vue
@@ -2,15 +2,15 @@
   <table>
     <thead>
     <tr>
-      <th v-for="header in headers" :key="header">
-        <p>{{ cap(header) }}</p>
+      <th v-for="header in headers" :key="headerField(header)">
+        <p>{{ headerLabel(header) }}</p>
       </th>
     </tr>
     </thead>
     <tbody>
     <tr v-for="item in items" :key="item[itemsKey]" :class="{clickable: !!action}" @click.once="act(item)">
-      <td v-for="header in headers" :key="header">
-        <p>{{ item[header] }}</p>
+      <td v-for="header in headers" :key="headerField(header)">
+        <p>{{ item[headerField(header)] }}</p>
       </td>
     </tr>
     </tbody>
@@ -25,11 +25,12 @@
     props: {
       headers: {required: true, type: Array,},
       items: {required: true, type: Array,},
-      itemsKey: {required: false, type: String, default: 'id'},
-      action: {required: false, type: Function}
+      itemsKey: {required: false, type: String, default: 'id',},
+      action: {required: false, type: Function, default: 'null',}
     },
     methods: {
-      cap: capitalize,
+      headerLabel: header => typeof(header) === 'object' ? header.label : capitalize(header),
+      headerField: header => typeof(header) === 'object' ? header.field : header,
       act: function(item) {
         if(!!this.action) this.action(item)
       },
